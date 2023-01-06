05649def53bdc448db38da25bcb08c1f292df3d1
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2023-01-06 18:34:19 +0400

    Create assigment2.js

---
+++ assigment2.js
@@ -0,0 +1,9 @@
+const isIsland = true;
+const population = 11;
+const language = "qartuli";
+
+if (population < 50 && !isIsland && language === "english") {
+    console.log('You should live in Georgia:)')
+} else {
+    console.log('Georgia does not meet your criteria :(')
+}
