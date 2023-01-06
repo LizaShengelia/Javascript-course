c86a909d5b1cc73784f9fbf27a31430d5917ab70
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2023-01-06 18:39:21 +0400

    Create assigment7.js

---
+++ assigment7.js
@@ -0,0 +1,13 @@
+function percentageOfWorld1(population) {
+    return population / 7900 * 100;
+}
+
+console.log(percentageOfWorld1(1441))
+
+
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
+const china2 = percentageOfWorld2(1441)
+
+console.log(china2)
