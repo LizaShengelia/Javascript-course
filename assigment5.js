ee9492d17ff642457f3b31ceb1303ef072b4a041
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2023-01-06 18:36:53 +0400

    Create assigment5.js

---
+++ assigment5.js
@@ -0,0 +1,6 @@
+const population = 2
+
+
+let result = population >= 33 ? "Portugal's population is above average" : "Portugal's population is below average";
+
+console.log(`${population >= 33 ? "Portugal's population is above average" : "Portugal's population is below average"}`)
