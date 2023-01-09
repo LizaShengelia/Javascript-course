63eab01c874ec5524be59143ade4ef5e7fcf4731
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2023-01-09 12:54:56 +0400

    Create ChallangeObjects.js

---
+++ ChallangeObjects.js
@@ -0,0 +1,9 @@
+const jonas = {
+    firstName: "jonas",
+    lastName: "kogua",
+    age: 2037 - 1991,
+    job: 'teacher',
+    friends: ['michael', 'peter', 'steven']
+}
+
+console.log(`${jonas.firstName} has a ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`)
