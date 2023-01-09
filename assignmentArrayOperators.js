91d4800f064e81721498d77af747cb7c81ff7799
liza shengelia <59509299+LizaShengelia@users.noreply.github.com>
2023-01-09 12:52:19 +0400

    Create assignmentArrayOperators.js

---
+++ assignmentArrayOperators.js
@@ -0,0 +1,19 @@
+const neighbours = ["turqeti", "azerbaijani", "ruseti"];
+
+neighbours.push("utopia")
+
+console.log(neighbours)
+
+neighbours.pop("utopia")
+console.log(neighbours)
+
+if (neighbours.includes("germany")) {
+    console.log('Probably not a central European country :D')
+}
+
+neighbours.indexOf("turqeti")
+
+neighbours[0] = "batumi"
+
+console.log(neighbours)
+
